cfa95f4fd2135a9f37947ab8e6778d81b3367c74
Bing Tong <k.thomas.tong@gmail.com>
2024-10-31 13:13:33 +0800

    feat:remove proxy switching (#3)

--- config/env.zsh
+++ config/env.zsh
@@ -1,21 +1,3 @@
 set -o vi
 export ENV_HOME="$HOME/.env"
 export PATH=/usr/local/bin:$PATH
-
-if [ 1 -eq $(lsof -ni -P | grep LISTEN | grep -c "7890") ]; then
-  export https_proxy=http://127.0.0.1:7890
-  export http_proxy=http://127.0.0.1:7890
-  export all_proxy=socks5://127.0.0.1:7890
-  ln -sf ~/.docker/config.proxy.json ~/.docker/config.json
-  if command -v npm &> /dev/null; then
-    npm config set proxy $http_proxy
-    npm config set https-proxy $https_proxy
-  fi
-else
-  unset https_proxy http_proxy all_proxy
-  ln -sf ~/.docker/config.nonproxy.json ~/.docker/config.json
-  if command -v npm &> /dev/null; then
-    npm config delete proxy
-    npm config delete https-proxy
-  fi
-fi
